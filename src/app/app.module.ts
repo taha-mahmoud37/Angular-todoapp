eac50d8ad5b6fa0aaf576bb6b9a8790c555d1aae
taha <taha29805@gmail.com>
2024-07-16 12:05:13 +0300

    initializaing app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListItemComponent } from './list-item/list-item.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListItemComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
